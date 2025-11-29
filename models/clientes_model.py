cd7cea99624b51f2ef074dc2a5fb56cafd3769e3
Zubin Juárez <zubinxxi@gmail.com>
2025-11-29 14:43:40 -0500

    Se crea el archivo con la clase modelo de clientes

---
+++ models/clientes_model.py
@@ -0,0 +1,26 @@
+from database import run_query, run_actions
+from datetime import date
+
+class ClientesModel:
+    def __init__(self, id="", cliente="", tel="", proyecto="", fecha=date.today()):
+        self.id = id
+        self.cliente = cliente
+        self.tel = tel
+        self.proyecto = proyecto
+        self.fecha = fecha  
+    
+    @staticmethod
+    def get_all():
+        return [ClientesModel(**cliente) for cliente in run_query("SELECT * FROM clientes")]
+    
+    @staticmethod
+    def create(cliente, tel, proyecto, fecha):
+        run_actions("INSERT INTO clientes (cliente, tel, proyecto, fecha) VALUES (%s, %s, %s, %s)", (cliente, tel, proyecto, fecha))
+    
+    @staticmethod
+    def update(id, cliente, tel, proyecto, fecha):
+        pass
+    
+    @staticmethod
+    def delete(id):
+        pass

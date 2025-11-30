19950fb12aba47d2942a3596ce1f6068b56f1f5c
Zubin Juárez <zubinxxi@gmail.com>
2025-11-29 23:22:01 -0500

    se agregó funcinalidad al boton de editar, se creó la función getId

--- views/alta_clientes.py
+++ views/alta_clientes.py
@@ -10,33 +10,46 @@ def alta_clientes():
         ss.clientes = []
 
     if "id" not in ss:
-        ss.id=""
+        ss.id = ""
+
+    def getId(id):
+        ss.id = id
+        
 
     _,col,_=st.columns([1,3,1])
     with col:
         st.title("Alta de Cliente")
         form_clientes(ss.id)
 
+    
+
+    # Fila de titulos de columnas
+    with st.container(border=True):
+        ccc1,ccc2,ccc3,ccc4,ccc5,ccc6 = st.columns(6)
+        ccc1.write("ID",)
+        ccc2.write("Cliente")
+        ccc3.write("Teléfono")
+        ccc4.write("Proyecto")
+        ccc5.write("Fecha")
+        ccc6.write("Acciones")
+    
     ss.clientes = ClientesModel.get_all()
 
     if ss.clientes:
         for cliente in ss.clientes:
             with st.container(border=True):
                 cc1,cc2,cc3,cc4,cc5,cc6 = st.columns(6)
-                with cc1:
-                    st.write(f"{cliente.id}")
-                with cc2:
-                    st.write(f"{cliente.cliente}")
-                with cc3:
-                    st.write(f"{cliente.tel}")
-                with cc4:
-                    st.write(f"{cliente.proyecto}")
-                with cc5:
-                    st.write(f"{cliente.fecha}")
-                with cc6:
-                    cl1,cl2,_ = st.columns([1,1,3])
-                    cl1.button("", key=f"delete_{cliente.id}", icon=":material/delete:", type="primary")
-                    cl2.button("", key=f"edit_{cliente.id}", icon=":material/edit:")
+                cc1.write(f"{cliente.id}")
+                cc2.write(f"{cliente.cliente}")
+                cc3.write(f"{cliente.tel}")
+                cc4.write(f"{cliente.proyecto}")
+                cc5.write(f"{cliente.fecha}")
+                cc6.button("Editar/Eliminar", key=f"edit_{cliente.id}", icon=":material/edit:", on_click=getId, args=(cliente.id,))
+                #with cc6:
+                    #cl1,cl2,_ = st.columns([1,1,3])
+                    #cl1.button("", key=f"edit_{cliente.id}", icon=":material/edit:", on_click=getId, args=(cliente.id,))
+                    #cl2.button("", key=f"delete_{cliente.id}", icon=":material/delete:", type="primary")
+                    
     else:
-        st.write("No hay clientes registrados")
+        st.caption("No hay clientes registrados en la base de datos")
     
